fa4c14881312cab34ea02cee7610ed43340779e5
muthumani11 <53254873+muthumani11@users.noreply.github.com>
2019-09-05 09:38:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Appname from './Appname';
+import './App.css';
+
+class App extends React.Component {
+
+render() {
+return (
+
+<div>
+<h1> Hello World </h1>
+<Appname name=" sara"/>
+<Appname name=" Michal"/>
+<Appname name=" ram"/>
+</div>
+);
+}
+}
+export default App;
